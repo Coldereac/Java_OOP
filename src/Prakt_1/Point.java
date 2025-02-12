eefb71664415f72bb66a410bb3c95569319e4818
Anton Malynevskyi <malina.elfg@gmail.com>
2025-02-12 21:44:42 +0100

    Point logic

--- src/Prakt_1/Point.java
+++ src/Prakt_1/Point.java
@@ -1,4 +1,28 @@
 package Prakt_1;
 
+
 public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean isInCircle(Point c, double r) {
+        return (Math.sqrt(Math.pow(this.x - c.x, 2) + Math.pow(this.y - c.y, 2))) <= r;
+    }
+
+    @Override
+    public String toString() {
+        return "Point(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Point)) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
 }
