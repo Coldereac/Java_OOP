a1b335243e2ebaa577307fcba768c52ea57ac87b
Anton Malynevskyi <malina.elfg@gmail.com>
2025-03-30 13:14:57 +0200

    Prakt_7

--- src/Prakt_7/Main.java
+++ src/Prakt_7/Main.java
@@ -6,24 +6,24 @@ public class Main {
     public static void main(String[] args) {
         try {
             // a) Getting Class object in three different ways
+            System.out.println("a) Getting Class object in three different ways:");
             Class<?> stringClass1 = String.class;
             Class<?> stringClass2 = Class.forName("java.lang.String");
             String str = "test";
             Class<?> stringClass3 = str.getClass();
             
-            System.out.println("a) Three ways to get Class object:");
-            System.out.println("1. String.class: " + stringClass1.getName());
-            System.out.println("2. Class.forName(): " + stringClass2.getName());
-            System.out.println("3. getClass(): " + stringClass3.getName());
+            System.out.println("Method 1: String.class -> " + stringClass1.getName());
+            System.out.println("Method 2: Class.forName() -> " + stringClass2.getName());
+            System.out.println("Method 3: getClass() -> " + stringClass3.getName());
             System.out.println();
 
-            // b) Printing class modifiers
+            // b) Getting class modifiers
             System.out.println("b) Class modifiers:");
             int modifiers = stringClass1.getModifiers();
             System.out.println("Modifiers: " + Modifier.toString(modifiers));
             System.out.println();
 
-            // c) Printing public constructors
+            // c) Getting public constructors
             System.out.println("c) Public constructors:");
             Constructor<?>[] constructors = stringClass1.getConstructors();
             for (Constructor<?> constructor : constructors) {
@@ -31,7 +31,7 @@ public class Main {
             }
             System.out.println();
 
-            // d) Printing all fields
+            // d) Getting all fields
             System.out.println("d) All fields:");
             Field[] fields = stringClass1.getDeclaredFields();
             for (Field field : fields) {
@@ -39,7 +39,7 @@ public class Main {
             }
             System.out.println();
 
-            // e) Printing public methods
+            // e) Getting public methods
             System.out.println("e) Public methods:");
             Method[] methods = stringClass1.getMethods();
             for (Method method : methods) {
@@ -48,7 +48,7 @@ public class Main {
             System.out.println();
 
             // f) Invoking a method and printing result
-            System.out.println("f) Invoking method and printing result:");
+            System.out.println("f) Invoking a method:");
             String testString = "Hello, Reflection!";
             Method toUpperCaseMethod = stringClass1.getMethod("toUpperCase");
             String result = (String) toUpperCaseMethod.invoke(testString);
