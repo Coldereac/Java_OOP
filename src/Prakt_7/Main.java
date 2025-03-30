5991caf4f9178fb0be53ab4473edeaec168de706
Anton Malynevskyi <malina.elfg@gmail.com>
2025-03-30 18:47:34 +0200

    Prakt 7 done

--- src/Prakt_7/Main.java
+++ src/Prakt_7/Main.java
@@ -1,63 +1,56 @@
 package Prakt_7;
 
+/*
+Протестувати методи рефлексії на типі String:
+a) отримати об'єкт Class трьома різними способами;
+b) вивести усі модифікатори класу String;
+c) вивести інформацію про всіх public конструктори класу String;
+
+d) вивести інформацію про всі поля класу String;
+e) вивести інформацію про всі public методи класу String;
+f) викликати будь-який із методів та роздрукувати результат.
+ */
+
 import java.lang.reflect.*;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            // a) Getting Class object in three different ways
-            System.out.println("a) Getting Class object in three different ways:");
-            Class<?> stringClass1 = String.class;
-            Class<?> stringClass2 = Class.forName("java.lang.String");
-            String str = "test";
-            Class<?> stringClass3 = str.getClass();
-            
-            System.out.println("Method 1: String.class -> " + stringClass1.getName());
-            System.out.println("Method 2: Class.forName() -> " + stringClass2.getName());
-            System.out.println("Method 3: getClass() -> " + stringClass3.getName());
-            System.out.println();
+    public static void main(String[] args) throws ClassNotFoundException {
+        //отримати об'єкт Class трьома різними способами
+        Class<?> string1 = String.class;
+        Class<?> string2 = "Hallo".getClass();
+        Class<?> string3 = Class.forName("java.lang.String");
 
-            // b) Getting class modifiers
-            System.out.println("b) Class modifiers:");
-            int modifiers = stringClass1.getModifiers();
-            System.out.println("Modifiers: " + Modifier.toString(modifiers));
-            System.out.println();
+        //вивести усі модифікатори класу String
+        System.out.println("Модифікатори String: " + Modifier.toString(string1.getModifiers()) + "\n");
 
-            // c) Getting public constructors
-            System.out.println("c) Public constructors:");
-            Constructor<?>[] constructors = stringClass1.getConstructors();
-            for (Constructor<?> constructor : constructors) {
-                System.out.println("Constructor: " + constructor);
-            }
-            System.out.println();
-
-            // d) Getting all fields
-            System.out.println("d) All fields:");
-            Field[] fields = stringClass1.getDeclaredFields();
-            for (Field field : fields) {
-                System.out.println("Field: " + field);
-            }
-            System.out.println();
-
-            // e) Getting public methods
-            System.out.println("e) Public methods:");
-            Method[] methods = stringClass1.getMethods();
-            for (Method method : methods) {
-                System.out.println("Method: " + method);
-            }
-            System.out.println();
-
-            // f) Invoking a method and printing result
-            System.out.println("f) Invoking a method:");
-            String testString = "Hello, Reflection!";
-            Method toUpperCaseMethod = stringClass1.getMethod("toUpperCase");
-            String result = (String) toUpperCaseMethod.invoke(testString);
-            System.out.println("Original string: " + testString);
-            System.out.println("Result of toUpperCase(): " + result);
-
-        } catch (Exception e) {
-            System.out.println("Error occurred: " + e.getMessage());
-            e.printStackTrace();
+        // вивести інформацію про всіх public конструктори класу String
+        System.out.println("Усі public конструкт String: ");
+        Constructor<?>[] constructors = string1.getConstructors();
+        for (Constructor<?> constructor : constructors) {
+            System.out.println(constructor);
         }
+        System.out.println();
+
+        //вивести інформацію про всі поля класу String;
+        System.out.println("Усі поля  String: ");
+        Field[] fields = string1.getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println(field);
+        }
+        System.out.println();
+
+        //вивести інформацію про всі public методи класу String
+        System.out.println("Усі public методи String: ");
+        Method[] methods = string1.getMethods();
+        for (Method method : methods) {
+            System.out.println(method);
+        }
+        System.out.println();
+
+        //викликати будь-який із методів та роздрукувати результат
+        String str = "          Hello, World!               ";
+        System.out.println("str = " + str);
+        System.out.println("Спробуем метод trim:");
+        System.out.println("str = " + str.trim());
     }
 }
